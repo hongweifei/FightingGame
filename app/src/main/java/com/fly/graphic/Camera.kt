0f5bd5944bc77f11a67c606160e0170c3be6c067
hongweifei <13071580229@163.com>
2020-04-03 21:48:54 +0800

    2020,4,3

--- app/src/main/java/com/fly/graphic/Camera.kt
+++ app/src/main/java/com/fly/graphic/Camera.kt
@@ -1,7 +1,7 @@
 package com.fly.graphic
 
 
-class Camera(var look_at_x : Float = 0f,var look_at_y : Float = 0f)
+class Camera(var look_at_x : Float = 0f,var look_at_y : Float = 0f)//摄像机坐标
 {
 
     init
@@ -9,6 +9,6 @@ class Camera(var look_at_x : Float = 0f,var look_at_y : Float = 0f)
 
     }
 
-    fun Move(x:Float,y:Float) { look_at_x = x;look_at_y = y }
+    fun Move(x:Float,y:Float) { look_at_x = x;look_at_y = y }//移动摄像机
 
 }
