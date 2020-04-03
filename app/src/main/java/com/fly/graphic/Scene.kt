c818eea9293aa952ff3c1fd91c96f7792ed967f2
hongweifei <13071580229@163.com>
2020-04-03 22:28:21 +0800

    2020,4,3

--- app/src/main/java/com/fly/graphic/Scene.kt
+++ app/src/main/java/com/fly/graphic/Scene.kt
@@ -13,26 +13,30 @@ import android.view.View
 import android.view.WindowManager
 import androidx.core.content.ContextCompat.getSystemService
 
-class Scene(context : Context?,attrs:AttributeSet?) : View(context,attrs)
+class Scene(context : Context?,attrs:AttributeSet?) : View(context,attrs)//场景类
 {
+    /*场景事件*/
     private var click:(event: MotionEvent?)->Unit = {}
     private var up:(event: MotionEvent?)->Unit = {}
     private var move:(event: MotionEvent?)->Unit = {}
 
+    /*渲染器及场景背景颜色*/
     private var scene_renderer : SceneRenderer? = null
     private var bg_color : Int = Color.WHITE
 
+    /*宽高比*/
     private val wm = context?.getSystemService(Context.WINDOW_SERVICE) as WindowManager
     private val dm = DisplayMetrics()
     protected var width_ratio:Float
     protected var height_ratio:Float
-    private var FPS:Float = wm.defaultDisplay.refreshRate
+    //private var FPS:Float = wm.defaultDisplay.refreshRate
 
     protected var scene_width = 16f
     protected var scene_height = 9f
 
     init
     {
+        /*初始化宽高比及场景渲染器*/
         wm.defaultDisplay.getMetrics(dm)
         width_ratio = dm.widthPixels / scene_width
         height_ratio = dm.heightPixels / scene_height
@@ -40,17 +44,18 @@ class Scene(context : Context?,attrs:AttributeSet?) : View(context,attrs)
         scene_renderer?.SetHeightRatio(height_ratio)
     }
 
+    /*场景绘制方法*/
     override fun onDraw(canvas: Canvas?)
     {
         super.onDraw(canvas)
 
-        FPS = wm.defaultDisplay.refreshRate
+        //FPS = wm.defaultDisplay.refreshRate
 
         //val begin_time = System.currentTimeMillis() // the time when the cycle begun
 
-        canvas?.drawColor(bg_color)
+        canvas?.drawColor(bg_color)//绘制场景背景颜色
 
-        if (canvas != null) { scene_renderer?.Display(canvas) }
+        if (canvas != null) { scene_renderer?.Display(canvas) }//调用渲染器渲染函数
 
         if(scene_renderer?.GetLayerType() != LAYER_TYPE_HARDWARE)
             setLayerType(LAYER_TYPE_SOFTWARE,scene_renderer?.GetPaint())
@@ -62,9 +67,10 @@ class Scene(context : Context?,attrs:AttributeSet?) : View(context,attrs)
 */
         //Log.e("FPS",FPS.toString())
 
-        postInvalidate()
+        postInvalidate()//不知道是什么，可让view再次绘制
     }
 
+    /*触摸事件*/
     @SuppressLint("ClickableViewAccessibility")
     override fun onTouchEvent(event: MotionEvent?): Boolean
     {
@@ -78,21 +84,25 @@ class Scene(context : Context?,attrs:AttributeSet?) : View(context,attrs)
         return true
     }
 
-    fun GetFPS() : Float { return wm.defaultDisplay.refreshRate }
+    //fun GetFPS() : Float { return wm.defaultDisplay.refreshRate }
 
+    /*获取场景成员*/
     fun GetSceneWidth() : Float { return scene_width }
     fun GetSceneHeight() : Float { return scene_height }
     fun GetSceneWidthRatio() : Float { return width_ratio }
     fun GetSceneHeightRatio() : Float { return height_ratio }
     fun GetSceneRenderer() : SceneRenderer? { return scene_renderer }
 
+    /*设置场景宽高*/
     fun SetSceneWidth(width:Float) { scene_width = width;width_ratio = dm.widthPixels / scene_width;scene_renderer?.SetWidthRatio(width_ratio) }
     fun SetSceneHeight(height:Float) { scene_width = height;height_ratio = dm.heightPixels / scene_height;scene_renderer?.SetHeightRatio(height_ratio) }
 
+    /*设置场景事件*/
     fun SetClick(click:(event: MotionEvent?)->Unit){ this.click = click }
     fun SetUp(up:(event: MotionEvent?)->Unit){ this.up = up }
     fun SetMove(move:(event: MotionEvent?)->Unit){ this.move = move }
 
+    /*设置场景渲染相关成员*/
     fun SetBackGroundColor(color:Int) { bg_color = color }
     fun SetCamera(camera: Camera){ scene_renderer?.SetCamera(camera) }
     fun SetSceneRenderer(r : SceneRenderer) { scene_renderer = r;scene_renderer!!.SetWidthRatio(width_ratio);scene_renderer!!.SetHeightRatio(height_ratio) }
--- app/src/main/java/com/fly/graphic/Sprite.kt
+++ app/src/main/java/com/fly/graphic/Sprite.kt
@@ -3,21 +3,22 @@ package com.fly.graphic
 import android.content.res.AssetManager
 import android.graphics.*
 
-open class Sprite(path:String? = null, asset_manager: AssetManager? = null)
+open class Sprite(path:String? = null, asset_manager: AssetManager? = null)//精灵类
 {
-    private var bitmap: Bitmap? = null
+    private var bitmap: Bitmap? = null//精灵bitmap
 
-    var width:Float = 0f
-    var height:Float = 0f
+    var width:Float = 0f//精灵宽
+    var height:Float = 0f//精灵高
 
-    private var render_animation = false
-    private var render_n:Int = 0
-    private var render_index = 0
-
-    private var rect_list:ArrayList<Rect> = ArrayList<Rect>()
+    /*序列帧动画*/
+    private var render_animation = false//精灵动画是否渲染
+    private var render_n:Int = 0//动画渲染次数
+    private var render_index = 0//动画渲染索引
+    private var rect_list:ArrayList<Rect> = ArrayList<Rect>()//序列帧动画分割坐标宽高？
 
     init
     {
+        /*设置精灵bitmap*/
         if(path != null)
         {
             if (asset_manager == null)
@@ -27,36 +28,45 @@ open class Sprite(path:String? = null, asset_manager: AssetManager? = null)
         }
     }
 
+    /*设置精灵bitmap*/
     fun SetBitmap(bitmap: Bitmap) { this.bitmap = bitmap }
     fun SetBitmap(bitmap_path: String)
     {
+        /*加载手机存储的图片*/
         val bitmap = BitmapFactory.decodeFile(bitmap_path)
         this.bitmap = bitmap
 
         width = bitmap.width.toFloat()
         height = bitmap.height.toFloat()
 
+        /*重置RectList*/
         rect_list.clear()
         rect_list.add(Rect(0,0, width.toInt(), height.toInt()))
     }
     fun SetBitmap(bitmap_path: String,asset_manager:AssetManager)
     {
+        /*加载assets的图片*/
         val bitmap = BitmapFactory.decodeStream(asset_manager.open(bitmap_path))
         this.bitmap = bitmap
 
         width = bitmap.width.toFloat()
         height = bitmap.height.toFloat()
 
+        /*重置RectList*/
         rect_list.clear()
         rect_list.add(Rect(0,0, width.toInt(), height.toInt()))
     }
+
+    /*设置RectList*/
     fun SetSrcRect(rect_list: ArrayList<Rect>) { this.rect_list = rect_list }
 
+    /*获取精灵的各项成员*/
     fun GetBitmap() : Bitmap? { return bitmap }
     fun GetSrcRect() : ArrayList<Rect> { return rect_list }
     fun GetSrcRect(index:Int) : Rect { return rect_list[index] }
     fun GetSrcRectF(index:Int) : RectF { val rectf:RectF = RectF(rect_list[index]);return rectf }
 
+    /*初始化SrcRect即RectList*/
     fun InitSrcRect(rect_list:ArrayList<Rect>) { this.rect_list.clear();this.rect_list = rect_list }
     fun InitSrcRect(start_x:Int,start_y:Int,width:Int,height:Int,horizontal:Int,vertical:Int)
     {
@@ -74,6 +84,7 @@ open class Sprite(path:String? = null, asset_manager: AssetManager? = null)
         }
     }
 
+    /*精灵渲染函数*/
     //fun render(canvas: Canvas, renderer:Renderer) { renderer.DrawSprite(canvas,this) }
     fun Render(canvas: Canvas, renderer:Renderer) { renderer.DrawSprite(canvas,this) }
     fun Render(canvas: Canvas, renderer: Renderer,x:Float,y:Float,width: Float = this.width,height: Float = this.height,index:Int = 0)
@@ -87,6 +98,7 @@ open class Sprite(path:String? = null, asset_manager: AssetManager? = null)
         if (render_index > end_index)
             render_animation = false
     }
+    /*渲染精灵动画*/
     fun RenderSpriteAnimation(canvas: Canvas,renderer: Renderer,x: Float,y: Float,width: Float = this.width,height: Float = this.height,begin_index:Int = 0,end_index:Int = 0,FPS:Int = 0)
     {
         if (!render_animation) { render_index = begin_index;render_animation = true }
